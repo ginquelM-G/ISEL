964eebafde1dbc4eba958eddfd7477c8b7aa7739
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-10-22 03:04:07 +0100

    Simple use of 'VolleyApp'

--- TMDb/app/src/main/java/pt/isel/pdm/tmdb/MainActivity.kt
+++ TMDb/app/src/main/java/pt/isel/pdm/tmdb/MainActivity.kt
@@ -6,21 +6,49 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File.separator
 
 class MainActivity : AppCompatActivity() {
 
-    var textBox : TextView? = null
+    private val requestQueue by lazy { Volley.newRequestQueue(this)}
+    private val textBox by lazy { findViewById(R.id.textBox) as TextView }
+    //val MovieSearchitem
+    private val SEARCH_MOVIE = "https://api.themoviedb.org/3/search/movie?api_key=b531994cdfaa8e3b441e4086b1c6756d&query=Batman"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textBox = findViewById(R.id.textBox) as TextView
+        //textBox = findViewById(R.id.textBox) as TextView
+
+        requestQueue.add(JsonObjectRequest(
+                SEARCH_MOVIE,
+                null,
+                {
+                    val jsonSearchMovieItem = it.get("results") as JSONArray
+
+
+                    Log.i("jsonSearchMovieItem:", jsonSearchMovieItem.toString())
+                    val searchMovieItem = jsonSearchMovieItem.asSequence().joinToString (
+                        separator = "\n",
+                        transform = { "Id:"+ it.getString("id")+ "\t\t" + it.getString("title")  }
+                    )
+
+                    textBox.text = searchMovieItem
+                    //textBox.setTextSize(50, 2.0f)
+                },
+
+                {
+                    textBox.text= "ERROR::\n${it.toString()}"
+                })
+
+        )
     }
 
 
-    public fun printSomething(view: View?){
-        val msg = "Message: " + (textBox?.getText() ?: "NONE")
-        Log.i("pdm/begin/main", msg)
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-    }
+    fun JSONArray.asSequence() =
+            (0 until length()).asSequence().map { get(it) as JSONObject }
 }
--- TMDb/app/src/main/java/pt/isel/pdm/tmdb/db/TheMovieDbClient.kt
+++ TMDb/app/src/main/java/pt/isel/pdm/tmdb/db/TheMovieDbClient.kt
@@ -5,14 +5,16 @@ package pt.isel.pdm.tmdb.db
  */
 class TheMovieDbClient {
 
-    val API_KEY : String = ""
-    val SEARCH_QUEY: String =""
+    val API_KEY : String = "b531994cdfaa8e3b441e4086b1c6756d"
+    val SEARCH_QUERY: String ="https://api.themoviedb.org/3/search/movie?api_key=%s&query=%s"
 
 
     /// Pesquisa de	filmes por nome
-    /// e.g.: https://developers.themoviedb.org/3/search/search-movies
-    fun search(title: String){
+    /// e.g.: https://api.themoviedb.org/3/search/movie?api_key=*****&query=war%20games
+    public fun search(title: String?){
 
-        //return null
+        var query: String = java.lang.String.format(SEARCH_QUERY, API_KEY, title)
+
+        println(query)
     }
 }
