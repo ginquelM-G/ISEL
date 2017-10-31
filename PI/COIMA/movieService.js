a169b05661b17719b49e242bc073b8e1f9aeaccd
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-10-31 15:46:21 +0000

    Implementing router

--- PI/COIMA/movieService.js
+++ PI/COIMA/movieService.js
@@ -5,10 +5,11 @@ const API_KEY = "b531994cdfaa8e3b441e4086b1c6756d";
 // sleepFor(2000)
 
 
-getMoviesByName('Batman', () =>{})
+//getMoviesByName('Batman', () =>{})
 
 module.exports = {
-
+    getMoviesByName,
+    getMoviesDetails
 }
 
 //1. https://api.themoviedb.org/3/search/movie?api_key=*****&name=war+games
@@ -29,14 +30,14 @@ function getMoviesDetails(movieId, cb){
     req(URI, (err, resp, data) =>{
         if(err) return cb(err)
         const movieDetails = JSON.parse(data.toString())
-        //console.log(movie.results[0])
+        console.log('id: '+movieDetails.id + '\nTitle: ' + movieDetails.title)
         cb(null, movieDetails)
     })
 }
 
 
 // 3. https://api.themoviedb.org/3/movie/860/credits?api_key=*****
-function getMoviesDetails(movieId, cb){
+function getMoviesDetailsss(movieId, cb){
     const URI = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`;
     req(URI, (err, resp, data) =>{
         if(err) return cb(err)
---
+++ PI/COIMA/movieWebApp.js
@@ -0,0 +1,80 @@
+const http = require('http')
+const url =  require('url')
+const service = require('./movieService')
+const port = 3010
+
+
+// Init HTTP server
+const server = http.createServer(router)
+server.listen(port)
+
+//Endpoints paths
+const movies = '/movies' // query-string ?name=.. 
+const listOfmovie = '/movies\/[0-9]+'
+const listOfPerson = '/movies\/[0-9]+/'
+const actors = '/actors\/[0-9]+' 
+
+function router(req, resp){
+    const urlObj = url.parse(req.url, true)
+    let action
+    //console.log('=> '+ urlObj.pathname)
+    //console.log('==> '+ urlObj.query.name)
+   
+
+    if(movies == urlObj.pathname || urlObj.query.name != undefined){
+        console.log('1. Pesquisa')        
+        action = cb => service.getMoviesByName(urlObj.query.name, cb)
+    }
+    if(listOfmovie == urlObj.pathname || urlObj.pathname.match(listOfmovie)){ 
+        console.log("2. Detalhes de um filme")
+        var movieId = urlObj.pathname.split('/')
+        //console.log(movieId[2])
+        action = cb => service.getMoviesDetails(movieId[2], cb)
+    }
+/*
+    if(movies == urlObj.pathname && urlObj.query!= undefined){
+        console.log("3. Lista de personagens de um filme")
+    }
+    if(actors  == urlObj.pathname || urlObj.pathname.match(actors)){
+         console.log("YESSSSSSS")
+    }
+    if(actors  == urlObj.pathname || urlObj.pathname.match(actors)){
+         console.log("Detalhes de uma pessoa")
+    }
+    */
+    if(action != undefined){ 
+        const obj = action((err, obj) =>{
+            let data 
+            if(err){
+                data = err.message
+                resp.statusCode = 500
+                console.log("ERROR: "+err.message)
+                //throw err
+            }else{
+                data = html(obj)
+                resp.statusCode = 200
+            }
+            resp.setHeader('Content-Type', 'text/html')
+            resp.end(data)
+        })
+    }
+    else{
+    //(resp != 200){
+        console.log(resp.toString())
+        resp.statusCode = 404 // Resource Not Found
+        resp.end()
+    }
+    //resp.end()
+
+}
+
+
+
+function html(obj){
+    let str = ''
+    for(let prop in obj){
+        const val = obj[prop]
+        str += `<li>${prop}:  ${val}</li>`
+    }
+    return `<ul>${str}</ul>`
+}
