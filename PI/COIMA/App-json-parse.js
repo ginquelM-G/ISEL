08b2274488275288b7eaa97f46b7e8eff5739cf9
gitAlexandre <39642@alunos.isel.ipl.pt>
2017-10-31 11:21:50 +0000

    Simple example using JSON PARSE

---
+++ PI/COIMA/App-json-parse.js
@@ -0,0 +1,26 @@
+const req = require('request')
+const API_KEY = "b531994cdfaa8e3b441e4086b1c6756d";
+
+//getLeagueLeader(445)
+// sleepFor(2000)
+
+
+getMoviesByName('Batman')
+
+//1. https://api.themoviedb.org/3/search/movie?api_key=*****&name=war+games
+function getMoviesByName(name){
+    const URI = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${name}`;
+    req(URI, getMoviesByNameAsync)
+}
+
+function getMoviesByNameAsync(err, resp, data){
+    if(err) throw err
+    const movie = JSON.parse(data.toString())
+    console.log(movie.results[0])
+}
+
+
+function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+}
