45e386f00f6cf1712af19634975eb1fe86dd8ff9
danielagomes <daniella.gomescrazy@gmail.com>
2017-11-26 16:28:05 +0000

    eliminates try catch

--- MovHubDb/TheMovieDbClient.cs
+++ MovHubDb/TheMovieDbClient.cs
@@ -15,51 +15,32 @@ namespace MovHubDb
         /// </summary>
         public MovieSearchItem[] Search(string title, int page)
         {
-            string path = "https://api.themoviedb.org/3/search/movie?api_key={0}&query={1}&page={2}";
-            try
-            {
-                string body = client.DownloadString(String.Format(path, API_KEY, title, page));
-                MovieSearch search = (MovieSearch) JsonConvert.DeserializeObject(body, typeof(MovieSearch));
-                return search.Results;
-            } catch(WebException ex)
-            {
-                return new MovieSearchItem[0];
-            }
-            
+            string path = "https://api.themoviedb.org/3/search/movie?api_key={0}&query={1}&page={2}";           
+            string body = client.DownloadString(String.Format(path, API_KEY, title, page));
+            MovieSearch search = (MovieSearch) JsonConvert.DeserializeObject(body, typeof(MovieSearch));
+            return search.Results;
         }
 
         /// <summary>
         /// e.g.: https://api.themoviedb.org/3/movie/508?api_key=*****
         /// </summary>
-        public Movie MovieDetails(int id) {
-            string path = "https://api.themoviedb.org/3/movie/{0}?api_key={1}";
-            try
-            {
-                string body = client.DownloadString(String.Format(path, id, API_KEY));
-                Movie search = (Movie)JsonConvert.DeserializeObject(body, typeof(Movie));
-                return search;
-            } catch(WebException ex)
-            {
-                return new Movie();
-            }
-            
+        public Movie MovieDetails(int id) 
+        {
+            string path = "https://api.themoviedb.org/3/movie/{0}?api_key={1}";            
+            string body = client.DownloadString(String.Format(path, id, API_KEY));
+            Movie search = (Movie)JsonConvert.DeserializeObject(body, typeof(Movie));
+            return search;   
         }
 
         /// <summary>
         /// e.g.: https://api.themoviedb.org/3/movie/508/credits?api_key=*****
         /// </summary>
-        public CreditsItem[] MovieCredits(int id) {
+        public CreditsItem[] MovieCredits(int id) 
+        {
             string path = "https://api.themoviedb.org/3/movie/{0}/credits?api_key={1}";
-            try
-            {
-                string body = client.DownloadString(String.Format(path, id, API_KEY));
-                Credits search = (Credits)JsonConvert.DeserializeObject(body, typeof(Credits));
-                return search.Cast;
-            } catch (WebException)
-            {
-                return new CreditsItem[0];
-            }
-
+            string body = client.DownloadString(String.Format(path, id, API_KEY));
+            Credits search = (Credits)JsonConvert.DeserializeObject(body, typeof(Credits));
+            return search.Cast;
         }
 
         /// <summary>
@@ -68,33 +49,20 @@ namespace MovHubDb
         public Person PersonDetais(int actorId)
         {
             string path = "https://api.themoviedb.org/3/person/{0}?api_key={1}";
-            try
-            {
-                string body = client.DownloadString(String.Format(path, actorId, API_KEY));
-                Person p = (Person)JsonConvert.DeserializeObject(body, typeof(Person));
-                return p;
-            } catch(WebException ex)
-            {
-                return new Person();
-            }
-
+            string body = client.DownloadString(String.Format(path, actorId, API_KEY));
+            Person p = (Person)JsonConvert.DeserializeObject(body, typeof(Person));
+            return p;
         }
 
         /// <summary>
         /// e.g.: https://api.themoviedb.org/3/person/3489/movie_credits?api_key=*****
         /// </summary>
-        public MovieSearchItem[] PersonMovies(int actorId) {
+        public MovieSearchItem[] PersonMovies(int actorId) 
+        {
             string path = "https://api.themoviedb.org/3/person/{0}/movie_credits?api_key={1}";
-            try
-            {
-                string body = client.DownloadString(String.Format(path, actorId, API_KEY));
-                PersonCredits pc = (PersonCredits)JsonConvert.DeserializeObject(body, typeof(PersonCredits));
-                return pc.Cast;
-            } catch(WebException ex)
-            {
-                return new MovieSearchItem[0];
-            }
-
+            string body = client.DownloadString(String.Format(path, actorId, API_KEY));
+            PersonCredits pc = (PersonCredits)JsonConvert.DeserializeObject(body, typeof(PersonCredits));
+            return pc.Cast;
         }
     }
 }
