3fca06902151b24af787fff0cf8f11590023b95b
Tony Mendes <tony.mendes@tekever.com>
2017-12-19 03:22:01 +0000

    removed old version

--- YAMD/app/src/main/java/yamd/g13/pdm/leic/isel/yamd/control/provider/Provider.kt
+++
@@ -1,7 +0,0 @@
-package yamd.g13.pdm.leic.isel.yamd.control.provider
-
-/**
- * Created by tony_mendes on 01/12/2017.
- */
-class Provider {
-}
--- YAMD/app/src/main/java/yamd/g13/pdm/leic/isel/yamd/model/Movie.kt
+++
@@ -1,8 +0,0 @@
-package yamd.g13.pdm.leic.isel.yamd.model
-
-import java.io.Serializable
-
-/**
- * Created by tony_mendes on 29/10/2017.
- */
-data class Movie (val id : Int, val poster_path : String) : Serializable{}
